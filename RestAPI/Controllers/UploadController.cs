87d95340db3e17d8f5e8ce915268e7445a2dc177
Nero <paul.bogza@gmail.com>
2024-09-10 10:41:50 +0200

    uploads endpoint

---
+++ RestAPI/Controllers/UploadController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc; 
+
+namespace RestAPI.Controllers;
+
+[ApiController]
+[Route("uploads")]
+public class UploadController : ControllerBase
+{    
+    [HttpPost]
+    public string Upload(string fileName)
+    {
+        return "upload successful";
+    }
+}
